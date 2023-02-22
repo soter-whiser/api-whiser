b1bc341ea686250f295dd4950ae0bd640c23c5b2
unknown <soter@whiser.com.br>
2023-02-22 18:00:35 -0300

    endpoint produto 3

--- pages/catavento/produto.js
+++ pages/catavento/produto.js
@@ -1,9 +1,11 @@
 async function produto(request, response) {
     const apiToken = process.env.API_TOKEN
 
-    const cataventoResponse = await fetch("http://catavento2.api.winbooksweb.com.br/BDIApi/Produto/Buscar?codigo=9788555342080" {
-                                            headers: {'API_TOKEN': API_TOKEN}
+    const cataventoResponse = await fetch('http://catavento2.api.winbooksweb.com.br/BDIApi/Produto/Buscar?codigo=9788555342080', {
+                                            headers: {'API_TOKEN': apiToken}
     })
+
+    response.setHeader('Cache-Control', 'smaxage=10, stale-while-revalidate')
                                             
     response.json({
         cataventoResponse
