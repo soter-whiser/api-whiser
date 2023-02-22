8e564e63066b0dc855e7920b4ea3cca0d0c5ac9d
unknown <soter@whiser.com.br>
2023-02-22 18:03:54 -0300

    remocao da api da catavento

--- pages/catavento/produto.js
+++ pages/catavento/produto.js
@@ -1,11 +1,9 @@
 async function produto(request, response) {
     const apiToken = process.env.API_TOKEN
 
-    const cataventoResponse = await fetch('http://catavento2.api.winbooksweb.com.br/BDIApi/Produto/Buscar?codigo=9788555342080', {
-                                            headers: {'API_TOKEN': apiToken}
-    })
 
-    response.setHeader('Cache-Control', 'smaxage=10, stale-while-revalidate')
+
+    response.setHeader('Cache-Control', 'smaxage=60, stale-while-revalidate')
                                             
     response.json({
         cataventoResponse
