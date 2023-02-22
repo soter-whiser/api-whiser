3dd9025a444ead19e7a00ab0014aad660f0d0523
unknown <soter@whiser.com.br>
2023-02-22 18:15:12 -0300

    edicao  da api da catavento

--- pages/catavento/produto.js
+++ pages/catavento/produto.js
@@ -2,15 +2,16 @@ async function produto(request, response) {
     const apiToken = process.env.API_TOKEN;
 
     const cataventoResponse = await fetch('http://catavento2.api.winbooksweb.com.br/BDIApi/Produto/Buscar?codigo=9788555342080', {
-                                            headers: {'API_TOKEN': apiToken}
-
-    });
+                                        headers: {
+                                            'API_TOKEN': apiToken,
+                                        },
+                                    });
     const cataventoResponseJson = await cataventoResponse.json();
 
     response.setHeader('Cache-Control', 'smaxage=60, stale-while-revalidate');
                                             
     response.json({
-        
+        cataventoResponseJson
     });
 }
 
